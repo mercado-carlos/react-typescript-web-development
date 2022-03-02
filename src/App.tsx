fe0b7285cd21c029d024197c8d24cc34e9340ae9
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-03-02 13:46:33 -0600

    Link and Navigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
 import './App.css';
 
-import AboutPage from './components/Routing/AboutPage';
-import HomePage from './components/Routing/HomePage';
+import AboutPage from './components/LinkAndRedirect/AboutPage';
+import HomePage from './components/LinkAndRedirect/HomePage';
+import ProfileProps from './components/LinkAndRedirect/ProfileProps';
 
 const App = () => {
     return (
         <BrowserRouter>
+            <Link replace to="/home">
+                Home
+            </Link>
+            <br />
+            <Link to="/about">About</Link>
+            <br />
+            <Link to="/profile">Profile</Link>
+            <br />
+            <Link to="/random">Random</Link>
             <Routes>
-                <Route path="/:username/about" element={<AboutPage />} />
-                <Route path="/" element={<HomePage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/profile" element={<ProfileProps />} />
+                <Route path="*" element={<Navigate to="/home" />} />
             </Routes>
         </BrowserRouter>
     );
---
+++ src/components/LinkAndRedirect/ProfileProps/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProfileProps = () => {
+    return (
+        <div>
+            <h1>Profile Props</h1>
+        </div>
+    );
+};
+
+export default ProfileProps;
