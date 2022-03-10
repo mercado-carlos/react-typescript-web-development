8a21b7358a87b4c3cad8fdb4bdb710aed016a34b
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-03-10 12:33:21 -0600

    Setting up Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './styles/App.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import HomePage from './containers/HomePage';
@@ -7,23 +9,31 @@ import AllProductsPage from './containers/AllProductsPage';
 import CheckoutPage from './containers/CheckoutPage';
 import { ROUTE } from './constants/route';
 import { HeaderNavigation } from './components/HeaderNavigation';
+import { rootReducer } from './store/rootReducer';
+
+const store = createStore(rootReducer);
 
 function App() {
     return (
-        <BrowserRouter>
-            <div className="app-container">
-                <HeaderNavigation />
-                <Routes>
-                    <Route path={ROUTE.HOME} element={<HomePage />} />
-                    <Route
-                        path={ROUTE.ALL_PRODUCTS}
-                        element={<AllProductsPage />}
-                    />
-                    <Route path={ROUTE.CHECKOUT} element={<CheckoutPage />} />
-                    <Route path="*" element={<Navigate to="/" />} />
-                </Routes>
-            </div>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <div className="app-container">
+                    <HeaderNavigation />
+                    <Routes>
+                        <Route path={ROUTE.HOME} element={<HomePage />} />
+                        <Route
+                            path={ROUTE.ALL_PRODUCTS}
+                            element={<AllProductsPage />}
+                        />
+                        <Route
+                            path={ROUTE.CHECKOUT}
+                            element={<CheckoutPage />}
+                        />
+                        <Route path="*" element={<Navigate to="/" />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </Provider>
     );
 }
 
